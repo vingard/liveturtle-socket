28119221e45dda69533512e3e84e39325ea58d56
EqualOwl <Danny13653@gmail.com>
2019-03-11 20:45:27 +0000

    Created Userinterface

--- client/client.py
+++ client/client.py
@@ -1,19 +1,32 @@
 import socket
- 
+import tkinter as tk
+
+host = "127.0.0.1"
+port = 5504
+global mySocket
+mySocket = socket.socket()
+mySocket.connect((host,port))
+
+def sendCommand():
+        
+        message = "print('test')"
+        mySocket.send(message.encode())  
+        commandNotSent = False
+
+        
+def InitilizeUI():
+        root = tk.Tk()
+        frame = tk.Frame(root)
+        frame.pack()
+
+        sendButton = tk.Button(frame, text="Send", command=sendCommand)
+        sendButton.pack(side=tk.LEFT)
+        
+
+        root.mainloop()
+        
 def Main():
-        host = "127.0.0.1"
-        port = 5504
-         
-        mySocket = socket.socket()
-        mySocket.connect((host,port))
-         
-        message = input(" -> ")
-         
-        while message != 'q':
-                mySocket.send(message.encode())     
-                message = input(" -> ")
-                 
-        mySocket.close()
- 
+        InitilizeUI()
+        
 if __name__ == '__main__':
     Main()
