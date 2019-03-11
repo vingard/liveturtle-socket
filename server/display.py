b96659fe36ebe226c60fdbfb9e1ab0ce19720358
Jake Green <jaketg10@gmail.com>
2019-03-11 20:46:33 +0000

    experimented with turtle, and display.py

---
+++ server/display.py
@@ -0,0 +1,8 @@
+import turtle
+
+
+x1 = turtle.Turtle()
+x2 = turtle.Turtle()
+x3 = turtle.Turtle()
+
+x3.forward(100)
--- server/server.py
+++ server/server.py
@@ -2,15 +2,17 @@ import socket
 from _thread import *
 import turtle
 import threading
+
 global teams
 global turtles
 teams = {}
 turtles = {}
 
+
 def runCode(code, team, addr):
     try:
-        turtle = turtles[addr]
-        turtle.color("blue")
+        #turtle = turtles[addr]
+        #turtle.color("blue")
         exec(code)
     except:
         print("Error from team "+str(team)+"!")
@@ -18,7 +20,7 @@ def runCode(code, team, addr):
 def on_new_client(csock, addr):
     print("Connection etablished from: " + str(addr))
     teams[addr] = len(teams) + 1
-    turtles[addr] = turtle.Turtle()
+    #turtles[addr] = turtle.Turtle()
     
     while True:
         data = csock.recv(8192).decode()
@@ -45,7 +47,7 @@ def Main():
 
     while True:
         conn, addr = mySocket.accept()
-        print(str(addr))
+        
         start_new_thread(on_new_client, (conn, addr))
         
     mySocket.close()
