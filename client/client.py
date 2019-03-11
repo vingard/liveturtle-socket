c0720fcb1783e310901347d447a28056d2d6b1b3
EqualOwl <Danny13653@gmail.com>
2019-03-11 23:04:24 +0000

    Client Now Sends TurtleCode.py to server

--- client/client.py
+++ client/client.py
@@ -1,28 +1,40 @@
 import socket
-import tkinter as tk
-
+#import tkinter as tk
+from tkinter import *
 host = "127.0.0.1"
 port = 5504
+
 global mySocket
+global Message
+message = "print('Error')"
 mySocket = socket.socket()
 mySocket.connect((host,port))
 
 def sendCommand():
-        
-        message = "print('test')"
+        message = open('turtleCode.py', 'r').read()
         mySocket.send(message.encode())  
-        commandNotSent = False
-
         
+def disable_event():
+    pass
+
+
+
 def InitilizeUI():
-        root = tk.Tk()
-        frame = tk.Frame(root)
+        root = Tk()
+        root.geometry("500x500")
+        frame = Frame(root)
         frame.pack()
-
-        sendButton = tk.Button(frame, text="Send", command=sendCommand)
-        sendButton.pack(side=tk.LEFT)
+        root.title("Client")
         
+        #Disabled During Debugging as its annoying
+        #root.protocol("WM_DELETE_WINDOW", disable_event)
 
+        #Creating Button
+        sendButton = Button(frame, text="Send", width="10", height="2", command=sendCommand)
+        sendButton.place(x=50, y=0)
+        sendButton.pack()
+        
+        
         root.mainloop()
         
 def Main():
