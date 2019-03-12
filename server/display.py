153592dbb0bef67dec75e7b3a4d55b574a837fce
Jake <jaketg10@gmail.com>
2019-03-12 15:14:27 +0000

    added function display

--- server/display.py
+++ server/display.py
@@ -1,8 +1,31 @@
 import turtle
+import os
+import time
+import tkinter
 
+global turtles
+global turtle
+turtles = {}
+display = turtle.Screen()
 
-x1 = turtle.Turtle()
-x2 = turtle.Turtle()
-x3 = turtle.Turtle()
+def runCode(code, team):
+    try:
+        exec(code)
+    except:
+        print("Error from team "+str(team)+"!")
+def reset():
+    print("Reset Screen...")
+    display.resetscreen()
 
-x3.forward(100)
+yurtle = turtle.Turtle()
+yurtle.color("white")
+while True:
+    displaycache = os.listdir("display/")
+    time.sleep(.2)
+    
+    for file in displaycache:
+        data = open("display/"+file, "r").read()
+        team = int(file)
+        runCode(data, team)
+
+        os.remove("display/"+file)
